df6367b1513515e54583d9c9e7fa6e677eb47616
luffy <prostonguentandung@gmail.com>
2026-05-02 17:37:17 +0300

    added game script, readme file and game screen

--- www/game.js
+++ www/game.js
@@ -1,6 +1,24 @@
 class mainScene {
   // The three methods currently empty
+  hit() {
+  // Change the position x and y of the coin randomly
+  this.coin.x = Phaser.Math.Between(100, 600);
+  this.coin.y = Phaser.Math.Between(100, 300);
 
+  // Increment the score by 10
+  this.score += 10;
+
+  // Display the updated score on the screen
+  this.scoreText.setText('score: ' + this.score);
+  // Create a new tween 
+    this.tweens.add({
+    targets: this.player, // on the player 
+    duration: 200, // for 200ms 
+    scaleX: 1.2, // that scale vertically by 20% 
+    scaleY: 1.2, // and scale horizontally by 20% 
+    yoyo: true, // at the end, go back to original scale 
+    });
+  } 
   preload() {
     // This method is called once at the beginning
     // It will load all the assets, like sprites and sounds  
@@ -23,10 +41,32 @@ class mainScene {
     // Display the score in the top left corner
     // Parameters: x position, y position, text, style
     this.scoreText = this.add.text(20, 20, 'score: ' + this.score, style);
+    this.arrow = this.input.keyboard.createCursorKeys();
+
  }
   update() {
     // This method is called 60 times per second after create() 
     // It will handle all the game's logic, like movements
+    // Handle horizontal movements
+    if (this.arrow.right.isDown) {
+    // If the right arrow is pressed, move to the right
+    this.player.x += 3;
+    } else if (this.arrow.left.isDown) {
+    // If the left arrow is pressed, move to the left
+    this.player.x -= 3;
+    } 
+
+    // Do the same for vertical movements
+    if (this.arrow.down.isDown) {
+    this.player.y += 3;
+    } else if (this.arrow.up.isDown) {
+    this.player.y -= 3;
+    } 
+    // If the player is overlapping with the coin
+    if (this.physics.overlap(this.player, this.coin)) {
+    // Call the new hit() method
+    this.hit();
+    }
   }
 }
 
